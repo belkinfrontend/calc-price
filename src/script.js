912beee2be89007223bd7b060612b638af782784
Dmitriy Belkin <belkin.frontend@gmail.com>
2018-06-20 13:02:14 +0300

    added function calculate and addEventListene

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
 // grab everything
 
+const price = document.getElementById('price');
+const quantity = document.getElementById('quantity');
+const total = document.getElementById('total');
 
 
 // create the function
 
+let calculate = () => {
+    let totalPrice = price.value * quantity.value;
+    //console.log(quantity.value);  
+    
+    total.innerText = `$ ${totalPrice.toFixed(2)}`;
+}
 
+// start page - start function
+
+calculate();
 
 // add the event listener
+
+price.addEventListener('input', calculate);
+quantity.addEventListener('input', calculate);
